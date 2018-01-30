1fff374f0bd560f3eaa7c34b04165fed4a2faba6
Ryan Dunton <ryan.dunton@area23hc.com>
2018-01-30 15:41:40 -0500

    webpacked for production

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ module.exports = {
   devtool: 'source-map',
   debug: true,
   entry: [
-    './src/index.js'
+    './src/index.js', './src/actions/index.js', './src/components/app.js', './src/components/chart.js', './src/components/google_map.js', './src/containers/search_bar.js', './src/containers/weather_list.js', './src/reducers/index.js', './src/reducers/reducer_weather.js'
   ],
   output: {
     path: __dirname,
